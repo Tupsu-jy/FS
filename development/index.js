0bb4754ffaf09931b3677c845c5ad100b723d971
Jaakko Ylinen <ylinenjaakko@gmail.com>
2022-12-14 19:42:33 +0200

    osa4 ke

--- development/index.js
+++ development/index.js
@@ -1,44 +1,18 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require('express');
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+require('dotenv').config();
 
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose.connect(process.env.MONGODB_URI);
 
-//const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(process.env.MONGODB_URI)
+app.use('/api/blogs', blogsRouter);
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
